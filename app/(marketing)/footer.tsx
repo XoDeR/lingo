9b1b2a8011b0ddc359dcdb9d984549f532fcf7b6
Volodymyr <XoDeR@users.noreply.github.com>
2024-05-30 19:18:02 +0200

    added different flags to the marketing footer

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,71 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/nl.svg"
+            alt="Dutch"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Dutch
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/es.svg"
+            alt="Spanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/gb.svg"
+            alt="English"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          English
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/fr.svg"
+            alt="French"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/de.svg"
+            alt="German"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          German
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/it.svg"
+            alt="Italian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+      </div>
     </footer>
   );
 };
