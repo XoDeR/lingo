9072c299ec2a69d34a19493d0e792a7d0ed6129b
Volodymyr <XoDeR@users.noreply.github.com>
2024-07-25 17:01:33 +0200

    visual representation of lessons

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -76,6 +76,24 @@ const main = async () => {
         order: 2,
         title: "Verbs",
       },
+      {
+        id: 3,
+        unitId: 1,
+        order: 3,
+        title: "Xxxx",
+      },
+      {
+        id: 4,
+        unitId: 1,
+        order: 4,
+        title: "Xxxx",
+      },
+      {
+        id: 5,
+        unitId: 1,
+        order: 5,
+        title: "Xxxx",
+      },
     ]);
 
     await db.insert(schema.challenges).values([
