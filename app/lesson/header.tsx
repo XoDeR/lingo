29ecc3e7e033b86909cbc17e31def5cd9a566cee
XoDeR <XoDeR@users.noreply.github.com>
2024-08-06 19:43:42 +0200

    added a progress bar and hearts counter to the quiz header

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,4 +1,6 @@
-import { X } from "lucide-react";
+import { Progress } from "@/components/ui/progress";
+import { InfinityIcon, X } from "lucide-react";
+import Image from "next/image"
 
 type Props = {
   hearts: number;
@@ -14,6 +16,22 @@ export const Header = ({ hearts, percentage, hasActiveSubscription }: Props) => 
         onClick={() => { }}
         className="text-slate-500 hover:opacity-75 transition cursor-pointer"
       />
+      <Progress
+        value={percentage}
+      />
+      <div className="text-rose-500 flex items-center font-bold">
+        <Image
+          src="/heart.svg"
+          height={28}
+          width={28}
+          alt="Heart"
+          className="mr-2"
+        />
+        {hasActiveSubscription
+          ? <InfinityIcon className="h-6 w-6 stroke-[3]" />
+          : hearts
+        }
+      </div>
     </header>
   )
 }
