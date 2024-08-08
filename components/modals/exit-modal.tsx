fc90ce4cbfdb11e88ef5d5a969016a0c850cad9d
XoDeR <XoDeR@users.noreply.github.com>
2024-08-08 21:19:21 +0200

    added dialog component

---
+++ components/modals/exit-modal.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useExitModal } from "@/store/use-exit-modal"
+
+export const ExitModal = () => {
+
+}
