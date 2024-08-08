110771fa1c58dd1c5bcba55a230b88eb1dbed5d7
XoDeR <XoDeR@users.noreply.github.com>
2024-08-08 22:06:07 +0200

    added exit-modal component

--- components/modals/exit-modal.tsx
+++ components/modals/exit-modal.tsx
@@ -16,5 +16,30 @@ import { Button } from "@/components/ui/button";
 import { useExitModal } from "@/store/use-exit-modal"
 
 export const ExitModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useExitModal();
 
+  useEffect(() => setIsClient(true), []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center w-full justify-center mb-5">
+            <Image
+              src="/mascot_sad.svg"
+              alt="Mascot"
+              height={80}
+              width={80}
+            />
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  )
 }
--- store/use-exit-modal.ts
+++ store/use-exit-modal.ts
@@ -7,7 +7,7 @@ type ExitModalState = {
 }
 
 export const useExitModal = create<ExitModalState>((set) => ({
-  isOpen: false,
+  isOpen: true,
   open: () => set({ isOpen: true }),
   close: () => set({ isOpen: false })
 }))
