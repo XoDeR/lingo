be9ac355aed2e73160066eb57723be880e538746
XoDeR <XoDeR@users.noreply.github.com>
2024-09-06 18:46:05 +0200

    card component styling

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,4 +1,6 @@
 import { challenges } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 type Props = {
   id: number;
@@ -15,8 +17,22 @@ type Props = {
 
 export const Card = ({ id, imageSrc, audioSrc, text, shortcut, selected, onClick, disabled, status, type }: Props) => {
   return (
-    <div>
-      Card
+    <div
+      onClick={() => { }}
+      className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
+        selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
+        selected && status === "correct"
+        && "border-green-300 bg-green-100 hover:bg-green-100",
+        selected && status === "wrong" && "border-rose-300 bg-rose-100 hover:bg-rose-100",
+        disabled && "pointer-events-none hover:bg-white",
+        type === "ASSIST" && "lg:p-3 w-full"
+      )}
+    >
+      {imageSrc && (
+        <div className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full">
+          <Image src={imageSrc} fill alt={text} />
+        </div>
+      )}
     </div>
   )
 }
